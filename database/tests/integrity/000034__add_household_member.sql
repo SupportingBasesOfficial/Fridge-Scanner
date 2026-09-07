fd29524f9deb353aac6fdd2e5848c10ffb5f466a
SupportingBasesOfficial <suportingbases@gmail.com>
2026-09-07 00:32:34 -0300

    test(be-03): guard household member mutation privileges

---
+++ database/tests/integrity/000034__add_household_member.sql
@@ -0,0 +1,36 @@
+-- FridgeScanner BE-03 integrity proof
+-- 000034__add_household_member.sql
+
+begin;
+
+do $$
+begin
+  if not has_function_privilege(
+    'fridge_app',
+    'fridge_internal.add_household_member(uuid,uuid,uuid,uuid,uuid,text)',
+    'EXECUTE'
+  ) then
+    raise exception 'fridge_app must be able to execute the governed add Household member mutation';
+  end if;
+
+  if has_function_privilege(
+    'fridge_worker',
+    'fridge_internal.add_household_member(uuid,uuid,uuid,uuid,uuid,text)',
+    'EXECUTE'
+  ) or has_function_privilege(
+    'fridge_readonly',
+    'fridge_internal.add_household_member(uuid,uuid,uuid,uuid,uuid,text)',
+    'EXECUTE'
+  ) then
+    raise exception 'worker/readonly capabilities must not mutate Household membership';
+  end if;
+
+  if has_table_privilege('fridge_app', 'fridge.household_membership', 'INSERT')
+     or has_table_privilege('fridge_app', 'fridge.household_membership', 'UPDATE')
+     or has_table_privilege('fridge_app', 'fridge.household_membership', 'DELETE') then
+    raise exception 'fridge_app must not receive direct Household membership DML';
+  end if;
+end;
+$$;
+
+rollback;
